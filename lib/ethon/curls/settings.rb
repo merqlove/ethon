9ea0ce215bb862e9fe723458473fe31a01933117
Alexander Merkulov <sasha@merqlove.ru>
2022-07-24 00:37:53 +0300

    Update settings.rb

--- lib/ethon/curls/settings.rb
+++ lib/ethon/curls/settings.rb
@@ -6,7 +6,7 @@ module Ethon
     callback :timer_callback, [:pointer, :long, :pointer], :multi_code
     callback :debug_callback, [:pointer, :debug_info_type, :pointer, :size_t, :pointer], :int
     callback :progress_callback, [:pointer, :long_long, :long_long, :long_long, :long_long], :int
-    ffi_lib_flags :now, :global
+    ffi_lib_flags :local, :now, :global
     ffi_lib ['libcurl', 'libcurl.so.4']
   end
 end
